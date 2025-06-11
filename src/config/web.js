e639aec4af29faa5ad214ed0d2d07e87235421ab
ThyThary <thythary123@gmail.com>
2025-06-11 11:50:09 +0700

    Modified style

--- src/config/web.js
+++ src/config/web.js
@@ -1,3 +1,3 @@
 const WEB_BASE_URL =
-  import.meta.env.REACT_APP_WEB_BASE_URL || "http://localhost:8012";
+  import.meta.env.REACT_APP_WEB_BASE_URL || "https://dict-dev.fmis.gov.kh:8083";
 export default WEB_BASE_URL;
